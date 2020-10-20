a8f2579f8844c4439605b2ed21e3779a0c8ac5af
Boiunets Denys <boyunets.dv@gmail.com>
2020-10-20 22:05:10 +0300

    first and second stage completed

--- Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,5 +1,13 @@
 package tictactoe
 
+import java.util.*
+
 fun main() {
-    // write your code here
+    val scanner = Scanner(System.`in`)
+    val values = scanner.nextLine()
+    println("---------")
+    println("| ${values[0]} ${values[1]} ${values[2]} |")
+    println("| ${values[3]} ${values[4]} ${values[5]} |")
+    println("| ${values[6]} ${values[7]} ${values[8]} |")
+    println("---------")
 }
